fe5b9a14e0dfdadc83da5dd7f28e99cbe8d65db3
Evan Kawa <timmypowergamer@gmail.com>
2017-07-23 22:36:20 -0600

    Initial Commit

---
+++ magicalmysterytour/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  magicalmysterytour
+//
+//  Created by Evan Kawa on 2017-07-23.
+//  Copyright © 2017 Evan Kawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
